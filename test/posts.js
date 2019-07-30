9d63a8078e6a240aa2da7c4005e194f67c4d82de
Murilo Dutra <murilodut@gmail.com>
2019-07-30 16:12:54 -0300

    Add test environment

---
+++ test/posts.js
@@ -0,0 +1,14 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+/* eslint-disable no-unused-vars */
+const should = chai.should();
+/* eslint-enable no-unused-vars */
+
+chai.use(chaiHttp);
+
+describe('Posts', function () {
+
+});
