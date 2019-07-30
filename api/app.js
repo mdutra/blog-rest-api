dedd7dcbfd8edf5271a236c1122d033490e91ceb
Murilo Dutra <murilodut@gmail.com>
2019-07-30 15:38:59 -0300

    Add MongoDB connection

--- api/app.js
+++ api/app.js
@@ -1,6 +1,19 @@
+const config = require('config');
+const debug = require('debug')('blog-api:server');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Mongoose connection
+const mongoUrl = config.get('mongoUrl');
+
+debug(`Connecting to ${mongoUrl}`);
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+  .catch(() => {
+    console.error('Unable to connect to MongoDB');
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
