3347d32f2982c7b9f43afe12244570eb0cd1243e
Murilo Dutra <murilodut@gmail.com>
2019-08-07 21:38:15 -0300

    Update populatedb script to add comments and users

--- populatedb.js
+++ populatedb.js
@@ -2,8 +2,10 @@ const config = require('config');
 const faker = require('faker');
 const mongoose = require('mongoose');
 
-const Author = require('./api/models/authorModel.js');
-const Post = require('./api/models/postModel.js');
+const Author = require('./api/models/authorModel');
+const Comment = require('./api/models/commentModel');
+const Post = require('./api/models/postModel');
+const User = require('./api/models/userModel');
 
 const mongoUrl = config.get('mongoUrl');
 
@@ -11,28 +13,52 @@ const mongoUrl = config.get('mongoUrl');
 
 faker.locale = 'pt_BR';
 
+function randomId(arr) {
+  return arr[Math.floor(Math.random() * arr.length)].id;
+}
+
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
   .then(() => Promise.all([
     Author.deleteMany({}),
     Post.deleteMany({}),
+    Comment.deleteMany({}),
+    User.deleteMany({}),
   ]))
   .then(() => {
+    const users = [...new Array(50)]
+      .map(() => new User({
+        name: faker.name.findName(),
+        email: faker.internet.exampleEmail(),
+      }));
+
     const authors = [...new Array(10)]
-      .map(() => ({
+      .map(() => new Author({
         firstName: faker.name.firstName(),
         lastName: faker.name.lastName(),
       }));
 
-    return Author.create(authors);
-  }).then((authors) => {
-    const posts = authors.map(({ _id }) => ({
-      title: faker.lorem.words(),
-      subtitle: faker.lorem.words(),
-      content: faker.lorem.paragraphs(),
-      authors: [_id],
-    }));
+    const comments = [...new Array(100)]
+      .map(() => new Comment({
+        content: faker.lorem.words(),
+        user: randomId(users),
+      }));
 
-    return Post.create(posts);
+    const posts = [...new Array(10)]
+      .map(() => new Post({
+        title: faker.lorem.words(),
+        subtitle: faker.lorem.words(),
+        content: faker.lorem.paragraphs(),
+        authors: [randomId(authors)],
+        comments: [...new Array(10)]
+          .map(() => randomId(comments)),
+      }));
+
+    return Promise.all([
+      ...(users.map(user => user.save())),
+      ...(authors.map(author => author.save())),
+      ...(comments.map(comment => comment.save())),
+      ...(posts.map(post => post.save())),
+    ]);
   })
   .then(() => {
     console.log(`Data stored in ${mongoUrl}`);
